052c87815ba3f1aca2c74e12e47ef75001389dd7
Sh4D0W7hr0neS <70110698+Sh4D0W7hr0neS@users.noreply.github.com>
2020-08-24 00:52:38 +0530

    Add files via upload

---
+++ src/com/shadowthrones/ciphers/EngFreqs.java
@@ -0,0 +1,16 @@
+package com.shadowthrones.ciphers;
+import java.math.*;
+
+public class EngFreqs {
+    static public double[] characterFreqs;
+    static double[] characterLogFreqs;
+    static double[] cha;
+    static{
+        characterFreqs = new double []{0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406,
+                0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074};
+        characterLogFreqs = new double[26];
+        for(int i = 0; i< 26; i++){
+            characterLogFreqs[i] = Math.log(characterFreqs[i]);
+        }
+    }
+}
